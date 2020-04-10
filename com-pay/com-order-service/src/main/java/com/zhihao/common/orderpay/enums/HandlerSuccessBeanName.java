411b756ea5bcc5d3979e81ddb7f43401d3c04136
陈志豪 <1040728129@qq.com>
2020-04-10 17:59:09 +0800

    修改说明 修改说明

--- com-pay/com-order-service/src/main/java/com/zhihao/common/orderpay/enums/HandlerSuccessBeanName.java
+++ com-pay/com-order-service/src/main/java/com/zhihao/common/orderpay/enums/HandlerSuccessBeanName.java
@@ -5,9 +5,11 @@ package com.zhihao.common.orderpay.enums;
  * @Date: 2020/4/10 11:56
  * @Description: 功能成功后业务逻辑实现类Bean名称
  * <p>
- *  此枚举对应的是支付功能异步通知成功支付状态后的业务功能处理,
- *  例如: 咖啡需要App支付功能, 先把异步通知支付成功后需要处理的业务功能逻辑,根据HandlerAfterSuccess规范
- *      接口进行实现,并需要指定实现类注册Bean的名称,并增加到此枚举。
+ *  例:  咖啡需要App支付功能:
+ *       1.先把异步通知支付成功后需要处理的业务功能逻辑,根据HandlerAfterSuccess规范接口进行实现,并需要指定实现类注册Bean的名称。
+ *       2.实现后把实现类注册Bean的名称添加到此枚举。并增加说明。
+ *       3.在调用{@link UnifiedPlaceAnOrder#placeAnOrder(PayType, String, String, String, String, String)}
+ *       (...Bean的名称)统一下单的时候传递对应此枚举的beanName名称传递过去
  * </p>
  * @Versions 1.0
  **/
